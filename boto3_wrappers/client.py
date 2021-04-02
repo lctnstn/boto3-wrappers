d058dcfee377dbad6fa70bcbb47b01b3d05d1809
Nathan Lichtenstein <nathan.lichtenstein@cbs.com>
2021-04-02 16:08:25 -0400

    Minor error handling.

--- boto3_wrappers/client.py
+++ boto3_wrappers/client.py
@@ -18,18 +18,23 @@ class client(object):
         self._aws_session_token = aws_session_token
 
     def _get_client(self, service):
+        try:
+            # If credentials are being passed through, use them
+            if not self._aws_access_key_id and not self._aws_secret_access_key:
+                session = boto3.session.Session(region_name=self._region_name)
+            # Default to local credentials
+            else:
+                session = boto3.session.Session(
+                    region_name=self._region_name,
+                    aws_access_key_id=self._aws_access_key_id,
+                    aws_secret_access_key=self._aws_secret_access_key,
+                    aws_session_token=self._aws_session_token
+                )
 
-        if not self._aws_access_key_id and not self._aws_secret_access_key:
-            session = boto3.session.Session(region_name=self._region_name)
-        else:
-            session = boto3.session.Session(
-                region_name=self._region_name,
-                aws_access_key_id=self._aws_access_key_id,
-                aws_secret_access_key=self._aws_secret_access_key,
-                aws_session_token=self._aws_session_token
-            )
+            self._name = session.client('iam').list_account_aliases()['AccountAliases'][0]
+            self._number = session.client('sts').get_caller_identity().get('Account')
 
-        self._name = session.client('iam').list_account_aliases()['AccountAliases'][0]
-        self._number = session.client('sts').get_caller_identity().get('Account')
-
-        return(session.client(service))
+            return(session.client(service))
+        except:
+            raise('Error creating client.')
+            exit(1)
